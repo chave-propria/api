d3197a1a93e1ddec2eeda0fd620056887a800bd3
pedro <contato0113@gmail.com>
2024-09-12 16:37:24 -0300

    Altera armazenamento de websocket adicionando chat_id como chave

--- chave_propria/utils/WebSocket_ConnectionManager.py
+++ chave_propria/utils/WebSocket_ConnectionManager.py
@@ -1,39 +1,45 @@
-from typing import Dict
+from typing import Dict, List
 
 from fastapi import WebSocket, WebSocketException, status
 
+
 class ConnectionManager:
-
-
     def __init__(self) -> None:
-        self.active_connections: Dict[str, WebSocket] = dict()
+        self.active_connections: Dict[str, List[Dict[str, WebSocket]]] = dict()
 
-
-    async def connect(self, websocket: WebSocket, current_user: str):
+    async def connect(
+        self, websocket: WebSocket, chat_id: str, current_user: str
+    ) -> None:
         # Evento de conexão
         await websocket.accept()
-        self.active_connections[current_user] = websocket
 
+        # Adiciona usuário no chat
+        if chat_id not in self.active_connections:
+            self.active_connections = {chat_id: list()}
 
-    async def personal_message(self, data: str, user: str):
+        self.active_connections[chat_id].append({current_user: websocket})
+
+    async def personal_message(self, chat_id: str, data: str, user: str):
         # Enviar mensagem para usuário requerido `user`
-        if user not in self.active_connections.keys():
+        recipient_user = {
+            user: connected_user.get(user)
+            for connected_user in self.active_connections.get(chat_id)
+            if user in connected_user.keys()
+        }
+        if not recipient_user:
             raise WebSocketException(
-                code=status.WS_1014_BAD_GATEWAY, reason='O destinatário não está conectado no momento!'
+                code=status.WS_1014_BAD_GATEWAY,
+                reason='O destinatário não está conectado no momento!',
             )
 
-        await self.active_connections[user].send_text(data=data)
+        await recipient_user[user].send_text(data=data)
 
-
-    
     async def broadcast(self, websocket: WebSocket, data: str):
         # Envia mensagem via websocket
-        for user, connection in self.active_connections.items():
+        for _, connection in self.active_connections.items():
             await connection.send_text(data=data)
 
-
-
-    def close_connection(self, user: str, websocket: WebSocket):
+    def close_connection(self, chat_id: str, user: str, websocket: WebSocket):
         self.active_connections.pop(user)
 
 
